1585aa4fc6995e7f122157a564852f7f2fd3f90c
Becheru888 <becheru93@gmail.com>
2019-06-28 09:20:41 +0100

    state maped

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { fetchSmurfs, addSmurf } from "../actions/index";
+import { bindActionCreators } from "redux";
 import './App.css';
+
+
+import styled from "styled-components";
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,7 +13,16 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  componentDidMount = () => {
+    this.props.fetchSmurfs();
+  }
+
+
   render() {
+    const { smurfs, fetchingSmurfs } = this.props;
+    if (fetchingSmurfs) {
+      return <div>Smurfs are coming :D</div>;
+    }
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
@@ -19,4 +34,32 @@ class App extends Component {
   }
 }
 
-export default App;
+
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+    fetchingSmurfs: state.fetchingSmurfs,
+    addingSmurf: state.addingSmurf,
+  };
+};
+
+const matchDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      fetchSmurfs,
+      addSmurf
+    },
+    dispatch
+  );
+};
+
+export default connect(
+  mapStateToProps,
+  matchDispatchToProps
+)(App);
+
+
+
+
+///Styled Component
+
