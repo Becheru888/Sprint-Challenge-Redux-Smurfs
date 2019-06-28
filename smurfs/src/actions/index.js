7488f5913d7903accd8b1702c3befad611ec6612
Becheru888 <becheru93@gmail.com>
2019-06-28 08:35:42 +0100

    actions creators added

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -20,3 +20,20 @@ export const ADD_FAIL = "ADD_FAIL";
    U - updateSmurf
    D - deleteSmurf
 */
+export const fetchSmurfs = () => dispatch => {
+  dispatch({ type: FETCH_START });
+  axios
+    .get("http://localhost:3333/smurfs")
+    .then(res => {
+      dispatch({ type: FETCH_SUCCESS, payload: res.data });
+    })
+    .catch(err => dispatch({ type: FETCH_FAIL, payload: err.message }));
+};
+
+export const addSmurf = smurf => dispatch => {
+  dispatch({ type: ADD_SMURF });
+  axios
+    .post("http://localhost:3333/smurfs/", smurf)
+    .then(res => dispatch({ type: ADD_SUCCESS, payload: res.data }))
+    .catch(err => dispatch({ type: ADD_FAIL, payload: err.message }));
+};
